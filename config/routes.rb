658344c15ce2145429da7ae6b457fdaf5e9ee2e3
Katsutoshi Kubota <wasurenbou.no.ki@gmail.com>
2016-07-05 23:29:34 +0900

    before upload Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'landingpage/index'
-
+  #get 'landingpage/index'
+  root 'landingpage#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
